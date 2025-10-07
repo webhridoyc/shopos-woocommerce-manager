4e770d08e3ffd192119d9d7e3715b2a1f40aa1ed
Hridoy Chondro <work.hridoy.chondro@gmail.com>
2025-10-08 00:38:55 +0600

    Create main plugin file

---
+++ shopos.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: ShopOS
+ * Description: Core Plugin for ShopOS WordPress Plugin.
+ * Version: 1.0
+ * Author: Your Name
+ */
+
+// Initialization code here.
+function shopos_init() {
+    // Code to create custom database tables and other setup.
+}
+add_action('init', 'shopos_init');
